3f6a31bee8567486d7528d439090cbdd901a7aa9
Lopatovszki Titán <55076178+Tanarursz@users.noreply.github.com>
2024-11-21 08:49:51 +0100

    (:

---
+++ dolgozat.py
@@ -0,0 +1,24 @@
+# Feladatok leírása
+feladatok = [
+
+    "1. Feladat: JSON fájl betöltése\n"
+    "   Írj egy függvényt, amely beolvassa a JSON fájlt, és egy Python listát ad vissza.\n\n",
+
+    "2. Feladat: Véletlenszerű állat kiválasztása\n"
+    "   Írj egy függvényt, amely véletlenszerűen kiválaszt egy állatot a betöltött listából.\n\n",
+
+    "3. Feladat: Játék mechanizmusának megírása\n"
+    "   Írj egy függvényt, amely a játékos válaszai alapján eldönti, hogy kitalálta-e az állatot.\n"
+    "   Kérdések formátuma: 'Az állat szőrös? (igen/nem)'\n\n",
+
+]
+
+"""
+Gondoltam egy állatra! Találd ki!
+Az állat ugat? (igen/nem): igen
+Az állat szőrös? (igen/nem): igen
+Az állat háziállat? (igen/nem): igen
+Gratulálok! Kitaláltad az állatot: kutya.
+
+"""
+
